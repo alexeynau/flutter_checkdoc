542f6f99052a565089fc71d0c2efc865b7beee87
Artem <artemrusskin92@gmail.com>
2024-04-13 22:47:34 +0300

    Dragg&drop

--- lib/presentation/widgets/dropzone_widget.dart
+++ lib/presentation/widgets/dropzone_widget.dart
@@ -1,5 +1,6 @@
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_dropzone/flutter_dropzone.dart';
 
 class DropzoneWidget extends StatefulWidget {
@@ -10,24 +11,27 @@ class DropzoneWidget extends StatefulWidget {
 class _DropzoneWidgetState extends State<DropzoneWidget> {
   late DropzoneViewController controller;
   bool isHovered = false;
-
+  bool isReady = false;
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: Container(
-        height: 250,
-        width: 250,
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: isHovered
-                ? const Color.fromARGB(255, 0, 0, 0)
-                : const Color.fromARGB(255, 255, 255, 255),
-            width: 2,
-          ),
-        ),
+        height: 200,
+        width: 200,
+        color: isHovered
+            ? Colors.grey
+            : ColorScheme.fromSeed(seedColor: Colors.deepPurple).surface,
+        // padding: const EdgeInsets.all(10),
+        // decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(10),
+        //   border: Border.all(
+        //     color: isHovered
+        //         ? Colors.green
+        //         : const Color.fromARGB(255, 255, 255, 255),
+        //     width: 2,
+        //   ),
+        // ),
         child: DottedBorder(
           borderType: BorderType.RRect,
           color: const Color.fromARGB(255, 0, 0, 0),
@@ -39,10 +43,15 @@ class _DropzoneWidgetState extends State<DropzoneWidget> {
               DropzoneView(
                 onCreated: (controller) => this.controller = controller,
                 onDrop: (ev) {
+                  setState(() {
+                    isReady = true;
+                  isHovered = false;
+                  });
+                  
                   print(ev);
                 },
                 onHover: () {
-                  setState(() {
+                  setState(() {Duration(seconds: 2);
                     isHovered = true;
                   });
                 },
@@ -52,37 +61,50 @@ class _DropzoneWidgetState extends State<DropzoneWidget> {
                   });
                 },
               ),
-              const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.upload_file,
-                      size: 100,
-                      color: Color.fromARGB(255, 0, 0, 0),
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          'Загрузите файл',
-                          style: TextStyle(
+              Center(
+                child: !isReady
+                    ? const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.upload_file,
+                            size: 100,
                             color: Color.fromARGB(255, 0, 0, 0),
-                            fontSize: 16,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Загрузите файл',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                '(.pdf, .docx, .xls)',
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 0, 0),
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    : Container(
+                        color: Colors.green,
+                        child: const Center(
+                          child: ClipRRect(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.done),
+                                Text(" Загружено"),
+                              ],
+                            ),
                           ),
                         ),
-                        Text(
-                          '(.pdf, .docx, .xls)',
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    ),
-                    
-                    
-                  ],
-                ),
+                      ),
               ),
             ],
           ),
