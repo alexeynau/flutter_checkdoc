96f230cb21a9aa3978d6548e397171e1618bbafe
alexeynau <firnen76@gmail.com>
2024-04-14 06:34:30 +0300

    fix

--- lib/domain/repository/user_repository.dart
+++ lib/domain/repository/user_repository.dart
@@ -5,14 +5,16 @@ import 'package:flutter_checkdoc/domain/entities/login_request.dart';
 import 'package:flutter_checkdoc/domain/entities/login_response.dart';
 import 'package:flutter_checkdoc/domain/entities/register_request.dart';
 import 'package:flutter_checkdoc/domain/entities/register_response.dart';
+import 'package:flutter_checkdoc/domain/entities/validate_response.dart';
 
 import '../../common/errors/failures.dart';
 import '../entities/document.dart';
+import '../entities/upload_file_response.dart';
 
 abstract class UserRepository {
   Future<Either<Failure, List<UserDocument>>> fetchDocuments();
-  Future<Either<Failure, String>> validateDocuments();
-  Future<Either<Failure, void>> uploadDocument(UserDocument document);
+  Future<Either<Failure, ValidateResponse>> validateDocument(String docId);
+  Future<Either<Failure, UploadFileResponse>> uploadDocument(UserDocument document);
   Future<Either<Failure, RegisterResponse>> register(RegisterRequest registerRequest);
   Future<Either<Failure, LoginResponse>> login(LoginRequest loginRequest);
 }
